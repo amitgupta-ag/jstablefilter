365b3a181c6817eb1936e3daf754f5adfc330333
Amit Gupta <amitgupta.nitb@gmail.com>
2014-08-03 02:31:16 +0530

    trunk
    comiting basecode

---
+++ README.txt
@@ -0,0 +1,9 @@
+Amit Gupta
+03-August-2014
+---------------
+A simple example to demonstrate how we can filter multiple rows of a plain html page based on multiple select dropdown box.
+
+please fork and update the example to make it better.
+
+please excuse dirty codes.
+----------------
---
+++ mytable.js
@@ -0,0 +1,38 @@
+function tablesort(){
+$("#searchselections").change(function(){
+var data = []; 
+$('#searchselections :selected').each(function(i, selected){ 
+  data[i] = $(selected).text(); 
+ 	});
+
+    //create a jquery object of the rows
+    var jo = $("#fbody").find("tr");
+    if (this.value == "") {
+        jo.show();
+        return;
+    }
+    //hide all the rows
+    jo.hide();
+
+    //Recusively filter the jquery object to get results.
+    jo.filter(function (i, v) {
+        var $t = $(this);
+        for (var d = 0; d < data.length; ++d) {
+            if ($t.is(":contains('" + data[d] + "')")) {
+                return true;
+            }
+        }
+        return false;
+    })
+    //show the rows that match.
+    .show();
+}).focus(function () {
+    this.value = "";
+    $(this).css({
+        "color": "black", "width":"100"
+    });
+    $(this).unbind('focus');
+}).css({
+    "color": "#C0C0C0"
+});
+}
